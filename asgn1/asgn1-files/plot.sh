2567ff1a3cd4579305c297e2782d612ec9582cd5
James Contini <jcontini@ucsc.edu>
2022-10-01 15:55:36 -0700

    Max length works now!

--- asgn1/asgn1-files/plot.sh
+++ asgn1/asgn1-files/plot.sh
@@ -6,17 +6,23 @@ make clean && make collatz
 
 #n x Length ###################
 echo > collatz_2.dat
-
-declare -i c
-
-for i in {2..10000}; do ./collatz -n $i | awk -b
+c=0
+for i in {2..1000}; do
+	./collatz -n $i > tmp_length.dat
+	while read line;
+	do
+		c=$((c+1))
+	done < tmp_length.dat
+	echo $c >> collatz_2.dat
+	c=0; 
+done
 
 gnuplot <<END
     set termoption enhanced
     set terminal pdf
-    set output "start_v_length.pdf"
-    set xrange [0:10000]
-    set yrange [0:100000]
+    set output "length.pdf"
+    set xrange [0:1000]
+    set yrange [0:300]
     set xlabel "{/:Italic n}"
     set ylabel "length"
     set zeroaxis
@@ -27,18 +33,24 @@ END
 #n x MaxValue ###################
 echo > collatz_3.dat
 
-for i in {2..10000}; do ./collatz -n $i | sort -n | tail -1 >> collatz_3.dat; done
+for i in {2..1000}; do ./collatz -n $i | sort -n | tail -1 >> collatz_3.dat; done
 
 gnuplot <<END
     set termoption enhanced
     set terminal pdf
-    set output "start_v_MaxVal.pdf"
-    set xrange [0:10000]
-    set yrange [0:100000]
+    set output "max_val.pdf"
+    set xrange [0:1000]
+    set yrange [0:10000]
     set xlabel "{/:Italic n}"
     set ylabel "value"
     set zeroaxis
     set title "Maximum Collatz Sequence Value"
     plot "./collatz_3.dat" with dots notitle
 END
+#Legnths x Frequency #################
+echo > collatz_4.dat
 
+while read num;
+do
+	
+done < tmp_length.dat
