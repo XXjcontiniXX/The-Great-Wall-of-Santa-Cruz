3eb4a049d03acbda46e00002cf4876164948917f
James Contini <jcontini@ucsc.edu>
2022-10-05 01:38:32 -0700

    sin func works

--- asgn2/asgn2-files/mathlib.c
+++ asgn2/asgn2-files/mathlib.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 #include "mathlib.h"
 
 
 
 int main(void) {
 	double d, L;
-	L = 3.14;
+	L = 3;
 	d = my_sin(L);
 	printf("Value is: %f\n", d);
 	return 0;	
@@ -26,28 +27,28 @@ static double Exp(double x) {
 
 
 double my_sin(double x) {
+	double total = 0;
+	double zero = 0;
 	double top = 1;
 	double bottom = 1;
-	double total = 0;
-	for (int n = 0; n < 17; n += 1) {
+	int prev_k = 0;
+	for (int n = 0; n < 11; n += 1) {
 		int k = (2 * n) + 1;
-		if (n % 2 == 0) {  	  // n is even
-			int e1 = (2 * n) + 1;  // to the e power
-			for (int m = 0; m < k; m += 1) { // x times itself m times (or e1 times)
-				top *= x;
-			}
-		
-		} else {		  //n is odd, top value is negative
-			for (int m = 0; m < k - 1; m += 1) {  // x times itself m times (or e2 times)
-				top *= x;
-				top = top - (top * 2);
-				printf("this is top: %f", top);
-			}	  
-		}
-		for (int c = k; c > 0; c -= 1) {
+		for (int c = k; c > prev_k; c -= 1) {
 			bottom *=c;
 		}
-		total += top / bottom;
+		for (; k > prev_k; prev_k += 1) { // x times itself m times (or k times) 
+			top *= x;
 		}
+		if (n%2 == 0) {  	// n is even
+			total += top / bottom; // adds to total
+			printf("neg this is top: %f, n = %d, k = %d, prev_k = %d\n", top, n, k, prev_k);
+		} else {		  //n is odd, top value is negative
+			total -= top / bottom; // subtracts from total
+		        printf("neg this is top: %f, n = %d, k = %d, prev_k = %d\n", top, n, k, prev_k);
+		}
+	prev_k = k;	
+	}
 return total;	
 }
+
