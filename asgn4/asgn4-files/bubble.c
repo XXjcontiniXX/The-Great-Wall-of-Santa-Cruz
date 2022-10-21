f935e154bbd059cdbb33cad4837697a3c54c3aa9
James Contini <jcontini@ucsc.edu>
2022-10-21 02:48:35 -0700

    bubble heap and shell completed

---
+++ asgn4/asgn4-files/bubble.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "stats.h"
+#include "bubble.h"
+
+
+// struct will be initialized and inputted from in test-harness
+
+void bubble_sort(Stats *stats, uint32_t *arr, uint32_t n_elements) {
+	for (uint32_t i = 0; i < n_elements; i += 1) {  // from element one -> last element 
+		int swapped = 0; // style is superior with pointer but saving arr for another idea
+		int *ptr_swapped = &swapped;
+		for (uint32_t j = n_elements - 1; j > i; j -= 1) { // iterate backwards from last element to the last previous unchecked element
+			if (arr[j] < arr[j-1]) {   // if the front element is lesser than than the behind element swap
+				swap(stats, &arr[j], &arr[j-1]); // first param needs to be address of struct, last params need to be adresses of swap elements
+				*ptr_swapped = 1;  // if throughout whole check through array does at least one swap then array is not done
+			
+			} 
+		
+		
+		}
+	       	if (!swapped) {  // if true that swaps is false (not found)
+			break; // actually finished / break main loop
+		
+		} 
+		
+	} 
+	
+}
---
+++ asgn4/asgn4-files/shell.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "stats.h"
+#include "bubble.h"
+#include <assert.h>
+
+uint32_t next_gap (uint32_t length) {
+	
+	int32_t n = length; // set a dynamic n
+	if (n == 1) {
+		return 0;
+	}
+	if (n <= 2) {
+		n = 1; // if n 2 or less n = 1 
+	}else{
+		n = (5 * n) / 11; // if n greater than 2 n = n/2 - some
+	}
+
+	return n;   	
+}
+
+
+
+
+
+void shell_sort(Stats *stats, uint32_t *arr, uint32_t n_elements) {
+	//printf("%lu", stats->moves);
+	for (uint32_t gap = next_gap(n_elements); gap > 0; gap = next_gap(gap)) {
+		//printf("%u", gap);
+		if ( gap == 0 ) {
+			break;
+		}
+		for (uint32_t i = gap; i < n_elements; i += 1) {
+			uint32_t j = i;
+			uint32_t temp = move(stats, arr[i]);
+			while (cmp(stats, j, gap) > -1 && cmp(stats, temp, arr[j-gap]) == -1) { // (j >= gap && temp < arr[j-gap]) /// if the elements on opposite sides of the gap are in the wrong spots keep switching by one gap backwards until its on the gap that it should stay on
+				arr[j] = move(stats, arr[j - gap]); // move it back one gap if its needs to be 
+				j -= gap;  // move back one gap back
+			
+			}
+			arr[j] = move(stats, temp); 
+		
+		}		
+		
+	} 
+	return;
+}
