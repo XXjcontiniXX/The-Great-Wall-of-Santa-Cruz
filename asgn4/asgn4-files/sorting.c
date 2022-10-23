933dd079b3cace71a117f4fea241d40eaaf12ee3
James Contini <jcontini@ucsc.edu>
2022-10-23 13:10:51 -0700

    half working test harness

--- asgn4/asgn4-files/sorting.c
+++ asgn4/asgn4-files/sorting.c
@@ -8,21 +8,130 @@
 #include "assert.h"
 #include "heap.h"
 #include "quick.h"
+#include "set.h"
+#include <unistd.h>
+#include <inttypes.h>
+
+#define OPTIONS "asbqhr:pn:H"
 
 
-
-int main(void) {
-	//Stats bubble;
-	Stats quick;
-        //uint32_t bubble_number[] = {10,3,1,4,7,4,3,2,8,10};
-	uint32_t quick_number[]	= {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 8,1,4,6,6,7,8,5,4,3,2,5,6,8,9,0,10,4,7};
-	//bubble_sort(&bubble, bubble_number, 10); // remember, n_elements is the number of element so if theres 10 elements this number is 10 **not** the max index number (9)
-	quick_sort(&quick, quick_number, 30);
-	
-	
-	for (int i = 0; i < 30; i+=1) { 
-		printf("%u\n", quick_number[i]);
+int main(int argc, char **argv) {
+        int opt = 0;
+	static uint32_t p_elements;
+	static uint32_t n_elements = 100;
+        mtrand_seed(13371453);
+	Set tracker = set_empty();	
+        while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+                switch(opt) {
+                case 'a': // all sorts
+			tracker = set_insert(tracker, 1);		
+			break;
+		case 's': // shell sort
+			tracker = set_insert(tracker, 2);
+			break;
+		case 'b': // bubble sort
+			tracker = set_insert(tracker, 3);
+			break;
+		case 'h': // heap sort
+			tracker = set_insert(tracker, 4);
+			break;
+		case 'q': // quick sort
+			tracker = set_insert(tracker, 5);
+			break;
+		case 'r': // seed	
+			mtrand_seed(strtoull(optarg, NULL, 10));
+			break;
+		case 'n': // optarg number of elements
+			if (strtoll(optarg, NULL, 10) > 2.5e8 || 0 > strtoll(optarg, NULL, 10)) { // greater than 0 and less or equal to 2.5e8
+                                printf("./sorting.c: error: seed must be positive and less than 2.5e8\n");
+                                exit(1);
+                        }
+			n_elements = strtoul(optarg, NULL, 10);
+			break;
+		case 'p': // prints out elements of the array
+			p_elements = strtoul(optarg, NULL, 10);
+			tracker = set_insert(tracker, 6);
+			break;
+		case 'H': // prints usage
+			printf("SYNOPSIS\n   A collection of comparison-based sorting algorithms.\n\nUSAGE\n   ./sorting [-Hasbhq] [-n length] [-p elements] [-r seed]\n\nOPTIONS\n   -H              Display program help and usage.\n   -a              Enable all sorts.\n   -b              Enable Bubble Sort.\n   -h              Enable Heap Sort.\n   -q              Enable Quick Sort.\n   -s              Enable Shell Sort.\n   -n length       Specify number of array elements (default: 100).\n   -p elements     Specify number of elements to print (default: 100).\n   -r seed         Specify random seed (default: 13371453).\n");	
+			exit(0);			
+			break;
+			
+		}
 	}
-	printf("%lu\n", quick.moves); 
+	if (set_member(tracker, 1)) {
+		printf("run all");
+	
+	}else{
+                uint32_t *array = (uint32_t *)malloc(n_elements * sizeof(uint32_t));
+		static uint64_t mask = 0x000000003fffffff;
+		for (uint32_t i = 0; i < n_elements; i++) {
+			array[i] = mask & mtrand_rand64();
+		}
 
+		if (set_member(tracker, 2)) { // bubble
+			uint32_t *array_s = (uint32_t *)malloc(n_elements * sizeof(uint32_t));
+			Stats shell;
+			for (uint32_t i = 0; i < n_elements; i++) {
+				array_s[i] = array[i];
+			}
+			shell_sort(&shell, array_s, n_elements);
+			for (uint32_t i = 0; i < n_elements; i++) {
+				printf("%u\n", array_s[i]);
+			}
+			free(array_s);
+			printf("shell\n");
+			
+		}
+		if (set_member(tracker, 3)) { // shell
+		        uint32_t *array_b = (uint32_t *)malloc(n_elements * sizeof(uint32_t));
+                        Stats bubble;
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                array_b[i] = array[i];
+                        }
+                        bubble_sort(&bubble, array_b, n_elements);
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                printf("%u\n", array_b[i]);
+                        }
+                        free(array_b);
+                        printf("bubble\n");
+		
+		} 
+		if (set_member(tracker, 4)) { // heap
+                        uint32_t *array_h = (uint32_t *)malloc(n_elements * sizeof(uint32_t));
+                        Stats heap;
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                array_h[i] = array[i];
+                        }
+                        heap_sort(&heap, array_h, n_elements);
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                printf("%u\n", array_h[i]);
+                        }
+                        free(array_h);
+                        printf("heap\n");
+		
+		} 
+		if (set_member(tracker, 5)) { // quick
+                        uint32_t *array_q = (uint32_t *)malloc(n_elements * sizeof(uint32_t));
+                        Stats quick;
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                array_q[i] = array[i];
+                        }
+                        quick_sort(&quick, array_q, n_elements);
+                        for (uint32_t i = 0; i < n_elements; i++) {
+                                printf("%u\n", array_q[i]);
+                        }
+                        free(array_q);
+			printf("quick\n");
+		
+		}
+		if (set_member(tracker, 6)) { // prints elements
+			printf("elements\n");
+
+		}
+
+
+		}
 }
+
+
