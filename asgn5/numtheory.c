aef0171250ff010ee90ce1ae3793fc7cf0c0722e
James Contini <jcontini@ucsc.edu>
2022-10-31 11:12:43 -0700

    broken is_prime

--- asgn5/numtheory.c
+++ asgn5/numtheory.c
@@ -1,9 +1,12 @@
 #include <stdbool.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <gmp.h>
 #include "numtheory.h"
-
+#include "randstate.h"
+#include <math.h>
+#include <assert.h>
 
 void gcd(mpz_t d, mpz_t a, mpz_t b) {
 	mpz_t t;
@@ -28,12 +31,7 @@ void gcd(mpz_t d, mpz_t a, mpz_t b) {
 
 void pow_mod(mpz_t o, mpz_t a, mpz_t d, mpz_t n) {
 	mpz_t v, p, m, new_v, new_p, new_d; // v and p exist
-	mpz_init(v);
-	mpz_init(p);
-	mpz_init(m);
-	mpz_init(new_v);
-	mpz_init(new_p);
-	mpz_init(new_d);
+	mpz_inits(v, p, m, new_v, new_p, new_d, NULL);
 	mpz_set_ui(v, 1); // v = 1
 	mpz_set(p, a); // p = a
 	while (mpz_cmp_ui(d, 0) > 0) { // d > 0
@@ -86,3 +84,72 @@ void mod_inverse(mpz_t o, mpz_t a, mpz_t n) {
 	gmp_printf("mod inverse of A = %Zd mod = %Zd is %Zd\n", a, n, o);
 
 }
+
+
+
+bool is_prime(mpz_t n, uint64_t iters) {
+	//initing
+	uint64_t s = 1;
+	mpz_t temp_y, y, a, pre_r, c, r, remainder, n_, m, two;
+	mpz_inits(temp_y, y, a, pre_r, c, r, remainder, n_, m, two, NULL);
+	//
+	mpz_sub_ui(n_, n, 1);
+	mpz_set_ui(two, 2);
+	//
+	uint64_t rand; 
+	//
+	while (true) {
+		s += 1;
+		mpz_mul_2exp(m, two, s); //  s = (s + 1) always //  real_m = m^s+1
+		mpz_mod(remainder, n_, m); // remainder = n - 1 (mod m^s+1)
+		if (!mpz_cmp_ui(remainder, 0)) { // remainder == 0
+			mpz_fdiv_q(c, n_, m);   // c = n-1/ 2^s+1
+			mpz_mul_ui(pre_r, c, 2); // pre_r = 2c
+			mpz_add_ui(r, pre_r, 1); // r = 2c + 1
+			break;
+		}
+	}
+	
+	for (uint64_t i = 1; i <= iters; i++) {
+		if ((rand = random()) > 2 && mpz_cmp_ui(n_, rand) > 0) { // if rand > 2 and n-1 > rand 
+			mpz_set_ui(a, rand); // a = rand
+			pow_mod(y, a, r, n); // y = pow_mod(a, r, n)
+			if (mpz_cmp_ui(y, 1) != 0 && mpz_cmp(y, n_) != 0) { // y != 1 y != n-1
+				uint64_t j = 1;
+				while (j <= s - 1 && mpz_cmp(y, n_) != 0) {
+					mpz_set(temp_y, y);
+					pow_mod(y, temp_y, two, n);
+					if (!mpz_cmp_ui(y, 1)) { // y == 1
+						return false;
+					}
+					j += 1;
+				}
+				if (mpz_cmp(y, n_) != 0) {
+					return false;
+				}
+			}	
+		}else{
+			assert(true);
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
