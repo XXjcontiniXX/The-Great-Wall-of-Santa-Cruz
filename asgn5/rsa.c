b1694942df5629d48cf20837938292a8e8d14b5d
James Contini <jcontini@ucsc.edu>
2022-11-03 15:13:03 -0700

    rsa_make_pub finished

--- asgn5/rsa.c
+++ asgn5/rsa.c
@@ -6,40 +6,46 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-
-int main (void) {
-	/*mpz_t oo, aa, dd, nn;
-	mpz_init(aa);
-	mpz_init(dd);
-	mpz_init(nn);
-	mpz_init(oo);
-	mpz_set_ui(aa, 3);
-	mpz_set_ui(dd, 128);
-	mpz_set_ui(nn, 7);
+void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t iters) {
+	uint64_t p_bits = random();
+	uint64_t q_bits;
+	mpz_t test, lambdan, pre_e, ee, gcd_e, pp, qq;
+	mpz_inits(test, lambdan, pre_e, e, gcd_e, pp, qq, NULL);
+	if ((nbits % 4) && !(p_bits % (nbits/2))) { // if n_bits not divisble by 4 and random # is 0
+		p_bits = (nbits/4) + (nbits % 4);
+	}else{
+		p_bits = (p_bits % (nbits/2)) + nbits/4;  // random in range 0-n_bits/2 then add n_bits/4
+	}
+	q_bits = nbits - p_bits; // q_bits is the difference
+	make_prime(p, p_bits, iters); // p holds a random number of p bits
+	make_prime(q, q_bits, iters); // q holds a random number of q bits
+	mpz_mul(n, p, q); // n hold pq
 	
-	pow_mod(oo, aa, dd, nn);
-	*/
-	/*
-	mpz_t b, f, g;
-	mpz_set_ui(f, 30);
-	mpz_set_ui(b, 45);
-	gcd(g, f, b); 
-*/ /*
- 	mpz_t o, a, n;
-	mpz_inits(o, a, n, NULL);
-        mpz_set_ui(a, 24);
-        mpz_set_ui(n, 4294967295);
-        mod_inverse(o, a, n);
-*//*
-	mpz_t crack;
-	mpz_init(crack);
-	randstate_init(613);	
-	mpz_set_ui(crack, 1031712);
-	printf("%d\n", is_prime(crack,200));
-*/
-	mpz_t p;
-	mpz_init(p);
-	randstate_init(611);
-	make_prime(p, 20, 200);
+	mpz_sub_ui(pp, p, 1);
+	mpz_sub_ui(qq, q, 1);	
+	gmp_printf("before gcd pp = %Zd qq = %Zd\n", pp, qq);
+	gcd(gcd_e, pp, qq);
+	mpz_mul(ee, pp, qq);
+	mpz_fdiv_q(lambdan, ee, gcd_e);
+	printf("p_bits = %lu ... q_bits = %lu\n", p_bits, q_bits);	
+	mpz_urandomb(pre_e, state, nbits); // generatre random number compare w/
+	gcd(test, pre_e, lambdan);
+	while (mpz_cmp_ui(test, 1)) { //gcd of random 
+		mpz_urandomb(pre_e, state, nbits);
+		gcd(test, pre_e, lambdan);
+	}
+	mpz_set(e, pre_e);
+	gmp_printf("e = %Zd ... lambdan = %Zd\n", e, lambdan);
+	return;
 
 }
+
+void rsa_write_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
+	pbfile = fopen("rsa.pub", "w");
+	//TODO
+
+}
+
+
+
+
