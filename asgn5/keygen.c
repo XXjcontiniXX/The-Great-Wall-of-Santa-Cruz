689bb070e0d3d092780ccfcfef3eaa097e0566ff
James Contini <jcontini@ucsc.edu>
2022-11-06 14:20:06 -0800

    options parsed correctly

--- asgn5/keygen.c
+++ asgn5/keygen.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include <gmp.h>
 #include "numtheory.h"
 #include "randstate.h"
@@ -7,45 +9,76 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-
-int main(void) {
-	mpz_t s, m, d, p, q, n, e;
-	mpz_inits(s, m, d, p, q, n, e, NULL);
-	FILE *pbfile;
-	FILE *pvfile;
-	FILE *ct;
-	FILE *message;
-	FILE *output;
-	ct = fopen("ct.txt", "w");
-	fclose(ct);
-	output = fopen("output.txt", "w");
-	fclose(output);
+#define OPTIONS "b:i:n:d:s:vh"
 
 
 
-	mpz_set_ui(m, 1000000); // m the message is 1000000 
-	randstate_init(100);
-	rsa_make_pub(p, q, n, e, 512, 200);
-	rsa_make_priv(d, e, p, q);
-	rsa_sign(s, m, d, n);
-	char *username = getenv("USER");
-	pbfile = fopen("rsa.pub", "w");
-	pvfile = fopen("rsa.priv", "w");
-	printf("%d\n", rsa_verify(m, s, e, n));
-	rsa_write_pub(n, e, s, username, pbfile);
-	rsa_write_priv(n, d, pvfile);
-	fclose(pbfile);
-	fclose(pvfile);
-	pbfile = fopen("rsa.pub", "r");
-	pvfile = fopen("rsa.priv", "r");
-	ct = fopen("ct.txt", "a");
-	message = fopen("message.txt", "r");
-	rsa_read_pub(n, e, s, username, pbfile); // reads from rsa.pub into n e s 
-	rsa_read_priv(n, d, pvfile); // reads from rsa.priv into n and d
-	rsa_encrypt_file(message, ct, n, e);
-	fclose(ct);
-	ct = fopen("ct.txt", "r");
-	output = fopen("output.txt", "a");
-	rsa_decrypt_file(ct, output, n, d);
+
+int main(int argc, char **argv) {
+        int opt = 0;
+	uint8_t v = 0;
+	//mpz_t p, q, e, n, s, d;
+	//mpz_inits(p, q, e, n, s, d, NULL);
+	char help[] = "Usage: ./keygen [options]\n  ./keygen-dist generates a public / private key pair, placing the keys into the public and private\n  key files as specified below. The keys have a modulus (n) whose length is specified in\n  the program options.\n    -s <seed>   : Use <seed> as the random number seed. Default: time()\n    -b <bits>   : Public modulus n must have at least <bits> bits. Default: 1024\n    -i <iters>  : Run <iters> Miller-Rabin iterations for primality testing. Default: 50\n    -n <pbfile> : Public key file is <pbfile>. Default: rsa.pub\n    -d <pvfile> : Private key file is <pvfile>. Default: rsa.priv\n    -v          : Enable verbose output.\n    -h          : Display program synopsis and usage.\n";	
+	char *pub_file = (char *)malloc(sizeof(char) * 8);
+	char *priv_file = (char *)malloc(sizeof(char) * 9);
+	char *new_pub_file;
+	char *new_priv_file;
+	strcpy(pub_file, "rsa.pub");
+	strcpy(priv_file, "rsa.priv");
+	uint64_t iters = 50;
+	uint64_t nbits = 1024;
+	uint64_t seed = (uint64_t)time(NULL);
+        while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+                switch(opt) {
+                case 'b': // nbits
+			nbits = strtol(optarg, NULL, 10);
+                        if (nbits < 50 || nbits > 4096) { 
+				fprintf(stderr, "./keygen: -b option must be between 50 and 4096 inclsuive\n");
+				v = v | (1 << 3); // 4th bit is nbits err
+			}
+                        break;
+                case 'i': // Miller iters
+                        iters = strtol(optarg, NULL, 10);
+			if (iters < 1 || iters > 500) {
+				fprintf(stderr, "./keygen: -i option must be between 1 and 500 inclsuive\n");
+				v = v | (1 << 2); // third bit is iters err
+			}
+			break;
+                case 'n': // *.pub
+                        new_pub_file = (char *)realloc(pub_file, strlen(optarg));
+			strcpy(new_pub_file, optarg);
+			pub_file = new_pub_file;
+			break;
+                case 'd': // *.priv 
+			new_priv_file = (char *)realloc(priv_file, strlen(optarg));
+                        strcpy(new_priv_file, optarg);
+                        priv_file = new_priv_file;
+                        break;
+                case 's': // seed
+                        seed = strtoul(optarg, NULL, 10);
+                        break;
+                case 'v': // verbose
+                   	v = v | (1 << 0); // first bit is verbose output    
+                        break;
+                case 'h': // stderr synopsis and usage
+                        v = v | (1 << 1); // second bit is -h output
+                        break;
+		default:
+			v = v | (1 << 4); // 5th bit is bad option
+			break;
+		} // switch
+	} // while
+	printf("v = %d\n", v);
+	if (v > 1) {
+		fprintf(stderr, "%s", help);
+		exit(1);
+	}
+	if (v == 1) { // only when v == 1 verbose  if v < 1 or 0 its chill
+		printf("iters: %lu\n nbits: %lu\n seed: %lu\n pub_file: %s\n priv_file: %s\n", iters, nbits, seed, pub_file, priv_file);
 	
+	}
+	randstate_init(seed); // inits w/ seed
+	printf("success\n");
+	return 0;
 }
