948b83a1542239d964b082d3d655cabe6766e4b7
James Contini <jcontini@ucsc.edu>
2022-11-21 13:01:06 -0800

    ht.c stats fixed

--- asgn6/ht.c
+++ asgn6/ht.c
@@ -6,6 +6,7 @@
 #include "ht.h"
 #include "city.h"
 #include <stdbool.h>
+
 struct HashTable {
 	uint64_t salt;
 	uint32_t size;
@@ -41,10 +42,11 @@ HashTable *ht_create(uint32_t size, bool mtf) {
 
 void ht_delete(HashTable **ht) {
 	for (uint32_t i = 0; i < ht_size(*ht); i++) {
-		ll_delete(((*ht)->lists) + i);
+		if (*(((*ht)->lists) + i) != NULL)  {
+			ll_delete(&(*(((*ht)->lists) + i)));
+		}
 	}
 	*ht = NULL;
-	
 }
 
 uint32_t ht_size(HashTable *ht) {
@@ -53,30 +55,54 @@ uint32_t ht_size(HashTable *ht) {
 
 Node *ht_lookup(HashTable *ht, char *oldspeak) {
 	uint64_t index = hash(ht->salt, oldspeak) % ht_size(ht); // index always in table - maybe empty tho
-	return ll_lookup(*((ht->lists) + index), oldspeak) == NULL ? NULL : ll_lookup(*((ht->lists) + index), oldspeak);
-	
+	uint32_t pre_seeks = 0;
+	uint32_t pre_links = 0;
+	uint32_t post_seeks = 0;
+	uint32_t post_links = 0;
+	LinkedList *ll = *((ht->lists) + index); // TODO check if appropiate to count node exams if no node was found?
+	if (ll != NULL) { // is ll we looking at NULL
+		ll_stats(&pre_seeks, &pre_links); // we can get the ll's stats if its not NULL
+		Node *ans = ll_lookup(ll, oldspeak);
+		if (ans == NULL) {
+			ht->n_misses += 1;
+		}else{
+			ht->n_hits += 1;
+		}
+		ll_stats(&post_seeks, &post_links); // after lookup stats
+		ht->n_examined += post_links - pre_links; // change stats
+		return ans; // return the node
+	} // ok then do nothing	
+	ht->n_misses += 1;
+	return NULL;
 }
 
 void ht_insert(HashTable *ht, char *oldspeak, char *newspeak) { 
 	uint64_t index = hash(ht->salt, oldspeak) % ht_size(ht);
+	uint32_t test = 0;
 	if (*((ht->lists) + index) == NULL) { // if spot is NULL make an ll
 		*((ht->lists) + index) = ll_create(ht->mtf);
 		ll_insert(*((ht->lists) + index), oldspeak, newspeak);
+		ht->n_keys += 1;
 	}else {
+		test = ll_length(*((ht->lists) + index));
 		ll_insert(*((ht->lists) + index), oldspeak, newspeak);
-	}
-}
-
-/*
-uint32_t ht_count(HashTable *ht) {
-	// returns *NON* NULL lls in hash table
-	for (uint32_t i = 0; i < ht_size(ht); i++) {
-		if () {
-		
+		if (test != ll_length(*((ht->lists) + index))) {
+			ht->n_keys += 1;
 		}
 	}
 }
-*/
+
+
+uint32_t ht_count(HashTable *ht) {
+	uint32_t count = 0;
+	for (uint32_t i = 0; i < ht_size(ht); i++) {
+                if (*((ht->lists) + i) != NULL)  {
+                        count += 1;
+                }
+        }
+	return count;
+}
+
 void ht_print(HashTable *ht) {
 	for (uint32_t i = 0; i < ht_size(ht); i++) {
 		if (*((ht->lists) + i) != NULL) {
@@ -85,4 +111,9 @@ void ht_print(HashTable *ht) {
 	}
 }
 
-void ht_stats(HashTable *ht, uint32_t *nk, uint32_t *nh, uint32_t *nm, uint32_t *ne);
+void ht_stats(HashTable *ht, uint32_t *nk, uint32_t *nh, uint32_t *nm, uint32_t *ne) {
+	*nk = ht->n_keys;
+        *nh = ht->n_hits;
+        *nm = ht->n_misses;
+        *ne = ht->n_examined;
+}
