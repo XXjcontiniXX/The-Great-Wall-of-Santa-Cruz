ca417ce2ff2e8c11a0ab296a15dfd8a1e8ffde58
jcontini <jcontini@ucsc.edu>
2022-11-27 01:06:44 -0800

    code.c (pop not working right)

---
+++ asgn7/asgn7-files/code.c
@@ -0,0 +1,114 @@
+#include "defines.h"
+#include "code.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+/*
+typedef struct {
+    uint32_t top;
+    uint8_t bits[MAX_CODE_SIZE];
+} Code;
+*/
+
+Code code_init(void) {
+	Code c;
+	c.top = 0;
+	for (uint32_t i = 0; i < MAX_CODE_SIZE; i++) {
+		c.bits[i] = 0;
+	}
+	return c; 
+
+}
+
+uint32_t code_size(Code *c) {
+	return c->top;
+			        
+}
+
+bool code_empty(Code *c) {
+	if (code_size(c) == 0) {
+                return true;
+        }else{
+                return false;
+        }
+}
+	
+bool code_full(Code *c) {
+	if (code_size(c) == ALPHABET) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool code_set_bit(Code *c, uint32_t i) {
+	uint32_t chunk = i / 8;
+        uint32_t index = i % 8;
+	if (index + chunk > 255) {
+                return false;
+        }
+	uint8_t one = 1;
+	c->bits[chunk] = c->bits[chunk] | one << index;
+	return true;
+}
+
+bool code_clr_bit(Code *c, uint32_t i) {
+	uint32_t chunk = i / 8;
+        uint32_t index = i % 8;
+	if (index + chunk > 255) {
+		return false;
+	}
+	uint8_t one = 1;
+        c->bits[chunk] = c->bits[chunk] & ~(one << index); // 0000.1000 -> 1111.0111
+	return true;
+}
+
+bool code_get_bit(Code *c, uint32_t i) {
+	uint32_t chunk = i / 8;
+        uint32_t index = i % 8;
+	uint8_t one = 1;
+	if (index + chunk > 255) {
+		return false;
+	}
+	uint8_t ans = c->bits[chunk] & one << index; 
+	return ans;
+}
+
+bool code_push_bit(Code *c, uint8_t bit) {
+	if (code_full(c)) {
+		return false;
+	}
+	uint32_t chunk = code_size(c) / 8;
+	uint32_t index = code_size(c) % 8;
+        c->bits[chunk] = c->bits[chunk] | bit << index;
+	c->top += 1;
+	return true;
+}
+
+bool code_pop_bit(Code *c, uint8_t *bit) {
+	if (code_empty(c)) {
+		return false;
+	}
+	uint32_t chunk = (code_size(c) - 1) / 8;
+        uint32_t index = (code_size(c) - 1) % 8;
+        c->top -= 1;
+	uint8_t ans = code_get_bit(c, chunk + index);
+	*bit = ans;
+	if (bit == NULL) {
+		fprintf(stderr, "Code.c: [ERROR] bit pointer is NULL.\n");
+	}
+	code_clr_bit(c, chunk + index);
+	return true;
+}
+
+void code_print(Code *c) {
+	for (uint32_t i = 0; i < ALPHABET; i++) {
+		if (i % 64 == 0) {
+                        printf("\n");
+                }
+                printf("%hu", code_get_bit(c, i));
+        }
+        printf("\n");
+        return;
+}
