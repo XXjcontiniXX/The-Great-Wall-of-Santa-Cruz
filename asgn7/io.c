dc8bc69cbbfe7240aa2061f25a624d0790bc78ff
James Contini <jcontini@ucsc.edu>
2022-12-01 14:10:30 -0800

    io OSTENSIBLY works, 00000000 goes away

--- asgn7/io.c
+++ asgn7/io.c
@@ -22,15 +22,15 @@ uint32_t loffset = 0;
 int read_bytes(int infile, uint8_t *buf, int nbytes) {
 	int i = 0;
 	int j;
-	int prev = 0;
+	//int prev = 0;
 	while (nbytes - i > 0 && (j = read(infile, buf + i, nbytes - i)) > 0) {
-		prev = j;
+		//prev = j;
 		i += j;
 	}
-	if (prev < 4096) { // and j = 0 which it must
+
+	/*if (prev < 4096) { // and j = 0 which it must
 		i--;
-	}
-	printf("i = %u\n", i);
+	}*/
 	bytes_read += i;
 	return i;	
 }
@@ -38,9 +38,14 @@ int read_bytes(int infile, uint8_t *buf, int nbytes) {
 int write_bytes(int outfile, uint8_t *buf, int nbytes) {
 	int i = 0;
 	int j;
-	while (nbytes - i > 0 && (j = write(outfile, buf + i, nbytes - i)) > 0) {  // when does read() not read the full block
-                i += j;
+	//printf("buf[0] = %u nbytes = %u\n", buf[0], nbytes);
+	
+	while (nbytes - i > 0 && (j = write(outfile, buf + i, nbytes - i)) > 0) {
+        	i += j;
+                printf("buffer not full case.\n");
+		printf("j = %u\n", j);
         }
+		
         bytes_written += i;
         return i;
 }
@@ -52,7 +57,7 @@ bool read_bit(int infile, uint8_t *bit) {
         }
 	if (offset >= BLOCK * 8 || offset == 0) { // 
 		last_byte = read_bytes(infile, buffer, BLOCK); // bufdex cannot be more than 4096
-		printf("new block:\nlast_byte = %u\n", last_byte);
+		//printf("last_byte = %u\n", last_byte);
 		/*
 		if (last_byte < 5) {
 			for (uint8_t i = 0; i < last_byte; i++) {
@@ -82,7 +87,9 @@ void write_code(int outfile, Code *c) {
 	if (code_empty(c)) { // if code size of c is 0 then quit
 		return;
 	}
-	
+	if (code_size(c) == 8) {
+		printf("woffset: %u\n", woffset);
+	}
        	woffset += code_size(c) - 1;
 	//printf("%u.", woffset);
 	loffset = woffset;
@@ -92,80 +99,85 @@ void write_code(int outfile, Code *c) {
 			loffset--;
 		}
 		woffset++;
+		printf("%u.", woffset);
+		if (loffset < 9) {
+			for (uint8_t i = 0; i < 8; i++) {
+				printf("%u", wbuffer[i/8] | (bit << (7 - i % 8)));
+			}
+			printf("\n");
+		}
+		if (woffset > 32767) {
+			flush_codes(outfile);
+		}
 	}else{
 		//printf("once");
 		wdiff = loffset - ((BLOCK * 8) - 1); // will always be positive
-		uint8_t i = code_size(c) - 1;
+		//uint8_t i = code_size(c) - 1;
+		uint8_t i = wdiff - 1;
 		while (loffset > (BLOCK * 8) - 1 && code_pop_bit(c, &bit)) { // copy backend which doesn't fit into buffer into bufbuf
 			//printf("twice\n");
 			bufbuf[i/8] = bufbuf[i/8] | (bit << (7 - i % 8)); // but when loffset finally reaches within the range of the block
+			//printf("bufbuf[%u//8]: %u\n", i/8, bufbuf[i/8]);
+			
+			for (uint8_t i = 0; i < 8; i++) {
+                         //       printf("bufbuf: %u bit %u\n", bufbuf[i/8] | (bit << (7 - i % 8)), bit);
+                        }
+			
 			i--;
 			loffset--; // then end loop
 		}
 		while (code_pop_bit(c, &bit)) { // pop remaining bits into actual buffer
-			printf("bit: %u\n", bit);
+			//printf("bit: %u chunk: %u index (0 is on right): %u loffset: %u\n", bit, loffset / 8, (7 - loffset % 8), loffset);
                         wbuffer[loffset/8] = wbuffer[loffset/8] | (bit << (7 - loffset % 8)); // TODO you have yet to test this
+			/*for (uint8_t i = 0; i < 8; i++) {
+				printf("wbuffer: %u\n", wbuffer[loffset/8] | (bit << (7 - loffset % 8)));
+			}*/
+			//printf("\n");
                         loffset--;
                 }
 		flush_codes(outfile); // flush and write that buffer
 			
 	}
-	/*
-	printf("%u\n", woffset);
-	if (woffset > BLOCK * 8) {
-		flush_codes(outfile);
-		printf("hey here once");
-		if (loffset > 0) { // if the buffer is full it'll flush the whole thing but if there is more bits in code
-			printf("not here tho");
-			woffset = loffset; // set the woffset to loffset which is how many bits r left
-			while (code_pop_bit(c, &bit)) { // pop the rest off
-                		wbuffer[loffset/8] = wbuffer[loffset/8] | (bit << (7 - loffset % 8));
-                		loffset--;
-        		}
-        		woffset++;
-		}
-
-	}
-	*/
 	return;
 }
 
 void flush_codes(int outfile) {
 	uint8_t byte = 0;
 	//uint8_t block = 0;
-	if (woffset > BLOCK * 8) {
+	//printf("happens after wbuffer\n");
+	if (woffset >= BLOCK * 8) {
 		//printf("woffset = %u\n", woffset);
-		write_bytes(outfile, wbuffer, (loffset / 8) + 1); // woffset is in the next byte, so go back one							
+		write_bytes(outfile, wbuffer, BLOCK); // woffset is in the next byte, so go back one							
 		while (wdiff % 8 != 0) {
 			bufbuf[wdiff / 8]  = bufbuf[wdiff / 8] | (byte << (7 - (wdiff % 8)));
 			wdiff++;
+			//printf("None or < 8 times\n");
 		}
-		write_bytes(outfile, bufbuf, (wdiff / 8) + 1);
+		//printf("bufbuf[0]: %u", bufbuf[1]);
+		if (woffset > BLOCK * 8) {
+			write_bytes(outfile, bufbuf, (wdiff / 8) + 1);
+		}
 		//woffset = 0;
 		//loffset = 0;
 
 	}else{
+		//printf("flushing the normal\n");
+		//printf("woffset for normal should be 128: %u\n", woffset);
+		if (woffset % 8 != 0) {
+			printf("woffset for normal should be 128: %u\n", woffset);
+			wbuffer[woffset / 8]  = wbuffer[woffset / 8] | (byte << (7 - (woffset % 8)));
+			woffset++;
+		}
 		
+		//printf("wbuffer[0] %u wbuffer[1] %u\n", wbuffer[0], wbuffer[1]);
+		write_bytes(outfile, wbuffer, woffset/8); // woffset never greater than 326
+	
 	}
 
 	// TODO dont forget about situation where only a couple codes have been put in that means 
 	// we should write starting at woffset to the end of that byte and then writebytes at woffset
 	// unless woffset % 8 because then the byte we want is full and its in a new one
-
-	/*	     //	                      |<--here, the woffset increase
-	if (woffset % 8 == 0) {
-		block = (woffset - 1) / 8;
-	}else{
-		block = woffset / 8;
-	}
-	while (woffset / 8 == block) { // while woffset is in the byte, zero then increment
-		wbuffer[block] = wbuffer[block] | (byte << (7 - woffset % 8));
-		woffset++;
-		printf("happen 8 times please\n");
-	}
-	//printf("woffset flush = %u\n", woffset);
-	//write_bytes(outfile, wbuffer, ((woffset - 1) / 8) + 1);
-	*/
+	printf("get here twice too\n");
 	woffset = 0;
 	return;
 }
