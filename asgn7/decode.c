548071d2f25124604598fc4eca0ee9cbd1dd806f
James Contini <jcontini@ucsc.edu>
2022-12-04 00:02:06 -0800

    decode has some stuff not even close to done yet

--- asgn7/decode.c
+++ asgn7/decode.c
@@ -18,8 +18,8 @@
 int main(int argc, char **argv) {
         char help[] = "SYNOPSIS\n  A Huffman encoder.\n  Compresses a file using the Huffman coding algorithm.\n\nUSAGE\n  ./huff-encode [-h] [-i infile] [-o outfile]\n\nOPTIONS\n  -h             Program usage and help.\n  -v             Print compression statistics.\n  -i infile      Input file to compress.\n  -o outfile     Output of compressed data.\n";
         uint8_t v = 0;
-        //int infile = 0;
-        //int outfile = 1;
+        int infile = 0;
+        int outfile = 1;
         int opt = 0;
         while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
                 switch (opt) {
@@ -30,14 +30,14 @@ int main(int argc, char **argv) {
                                 v = v | (1 << 1);
                                 break;
                         case 'o':
-
-                               // if ((outfile = open(optarg, O_WRONLY)) < 0) {
-                                 //       outfile = open(optarg, O_WRONLY | O_CREAT);
-                                //}
+                               if ((outfile = open(optarg, O_WRONLY)) < 0) {
+                                      outfile = open(optarg, O_WRONLY | O_CREAT);
+                               }
 
                                 break;
                         case 'i':
-                                //infile = open(optarg, O_RDONLY);
+
+                                infile = open(optarg, O_RDONLY);
                                 break;
                         default:
                                 fprintf(stderr, "%s", help);
@@ -46,5 +46,25 @@ int main(int argc, char **argv) {
 
                 }
         }
+	
+	if (v > 1) {
+                fprintf(stderr, "%s", help);
+                return 0;
+        }
+	
+	// going to read in bytes from infile into the header
+	union {
+                uint8_t bytes[sizeof(Header)];
+                Header h;
+        } hu;
+	
+	read_bytes(infile, hu.bytes, sizeof(Header));
+	
+	printf("hu.b[0]: %u\n", hu.bytes[0]);
+	
+			
+	
+	
+	
 	return 0;
 }
